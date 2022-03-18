d87105f9a376da3f1b397f47b939b73ba557ad72
Nathanael <sargishovsepyan2001@gmail.com>
2022-03-18 15:15:07 -0400

    microshell

--- exam_rank04/microshell.c
+++ exam_rank04/microshell.c
@@ -1,34 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <sys/wait.h>
 
-typedef	struct	s_cmd
+typedef struct	s_cmd
 {
 	char	**args;
 	int		in;
 	int		out;
 }				t_cmd;
 
-typedef	struct	s_micro
+typedef struct	s_micro
 {
 	t_cmd	*cmds;
-	char	**env;
 	pid_t	*fam;
+	char	**env;
 	int		segments;
 	int		n;
 }				t_micro;
 
 t_micro	g_data;
 
-void	ft_puterr(char	*msg)
+int	ft_strlen(char *str)
 {
-	write(2, msg, strlen(msg));
+	int	len;
+
+	len = 0;
+	if (!str)
+		return (len);
+	while (str[len])
+		len++;
+	return (len);
 }
 
-void	ft_exit(char *msg, int k)
+void	ft_puterr(char *msg)
+{
+	write(2, msg, ft_strlen(msg));
+}
+
+int	ft_exit(char *msg, int k)
 {
 	ft_puterr(msg);
 	if (k)
@@ -36,10 +47,10 @@ void	ft_exit(char *msg, int k)
 	exit(1);
 }
 
-void	ft_exec(t_cmd *cmd)
+int	ft_exec(t_cmd *cmd)
 {
-	if (dup2(cmd->in, 0) == - 1 || dup2(cmd->out, 1) == -1)
-		ft_exit("error: fatal", 1);
+	if (dup2(cmd->in, 0) == -1 || dup2(cmd->out, 1) == -1)
+		ft_exit("error: fatal\n", 1);
 	execve(cmd->args[0], cmd->args, g_data.env);
 	ft_puterr("error: cannot execute ");
 	ft_puterr(cmd->args[0]);
@@ -51,43 +62,44 @@ int	main(int argc, char **argv, char **environ)
 {
 	int	i;
 
-	// Initializing 
+	// Initializing
+
 	(void)argc;
 	i = 0;
 	g_data.env = environ;
 	g_data.segments = 1;
-
 	while (argv[i])
 		if (!strcmp(argv[i++], ";"))
 			g_data.segments++;
 	argv++;
 
-	// Main Loop
+	//Running the main loop
+
 	while (*argv && g_data.segments)
 	{
 		if (!strcmp(*argv, ";"))
 		{
 			g_data.segments--;
 			argv++;
-			continue;
+			continue ;
 		}
 
 		// Parsing the input
+
 		i = 0;
 		g_data.n = 1;
 		while (argv[i] && strcmp(argv[i], ";"))
-			if (!strcmp(argv[i++], "|"))
+			if (!strcmp("|", argv[i++]))
 				g_data.n++;
-		
 		g_data.cmds = malloc(sizeof(t_cmd) * g_data.n);
 		g_data.fam = malloc(sizeof(pid_t) * g_data.n);
 		if (!g_data.cmds || !g_data.fam)
-			ft_exit("error: fatal", 0);
+			ft_exit("error: fatal\n", 0);
 
-		i = 0;
-		int		cur = 0;
+		int 	cur = 0;
 		int		cont = 1;
 		char	**bag = argv;
+		i = 0;
 		while (cont)
 		{
 			g_data.cmds[cur].args = bag;
@@ -97,7 +109,6 @@ int	main(int argc, char **argv, char **environ)
 			{
 				if (!bag[i] || !strcmp(bag[i], ";"))
 					cont = 0;
-
 				bag[i] = NULL;
 				bag += i + 1;
 				cur++;
@@ -107,7 +118,8 @@ int	main(int argc, char **argv, char **environ)
 				i++;
 		}
 
-		// Forking, Pipeing and Execution
+		// Forking & Piping & Exexcution
+
 		if (strcmp(g_data.cmds[0].args[0], "cd"))
 		{
 			i = 0;
@@ -118,16 +130,15 @@ int	main(int argc, char **argv, char **environ)
 					int	pipefd[2];
 
 					if (pipe(pipefd))
-						ft_exit("error: fatal", 0);
+						ft_exit("error: fatal\n", 0);
 					g_data.cmds[i].out = pipefd[1];
-					g_data.cmds[i + 1].in = pipefd[0]; 
+					g_data.cmds[i + 1].in = pipefd[0];
 				}
 				g_data.fam[i] = fork();
 				if (g_data.fam[i] == -1)
-					ft_exit("error: fatal", 1);
+					ft_exit("error: fatal\n", 1);
 				else if (g_data.fam[i] == 0)
 					ft_exec(g_data.cmds + i);
-
 				waitpid(g_data.fam[i], NULL, 0);
 				if (g_data.cmds[i].in != 0)
 					close(g_data.cmds[i].in);
@@ -139,9 +150,8 @@ int	main(int argc, char **argv, char **environ)
 		else
 		{
 			if (!g_data.cmds[0].args[1] || g_data.cmds[0].args[2])
-				ft_puterr("error: cd: bad arguments");
+				ft_puterr("error: cd: bad arguments\n");
 			else
-			
 				if (chdir(g_data.cmds[0].args[1]) == -1)
 				{
 					ft_puterr("error: cd: cannot change directory to ");
@@ -150,18 +160,17 @@ int	main(int argc, char **argv, char **environ)
 				}
 		}
 
-		// Ending
+		// Ending 
+
 		if (g_data.segments > 1)
 		{
 			i = 0;
-			while (g_data.cmds[g_data.n - 1] .args[i])
+			while (g_data.cmds[g_data.n - 1].args[i])
 				i++;
 			argv = &g_data.cmds[g_data.n - 1].args[i] + 1;
 		}
-		free(g_data.fam);
 		free(g_data.cmds);
+		free(g_data.fam);
 		g_data.segments--;
 	}
-
-	return (0);
 }
